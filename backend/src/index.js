4738655c09967694638be6cab980b7a48f2f76e2
Saksham Manocha <sakshammanocha14@gmail.com>
2020-07-04 21:16:45 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('../db/mongoose');
 
 const userRouter = require('./routers/user');
+const messageRouter = require('./routers/message');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use('/users',userRouter);
+app.use('/messages',messageRouter);
 
 app.listen(port,console.log(`Server is running at port ${port}!!!`))
